e220012e0dc55ef901dbce9ce501ca9dc7832700
Kunal Parkhade <kunalparkhade@gmail.com>
2024-11-07 19:44:02 +0530

    Prediction Pipeline and transform attribute error solved

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -49,7 +49,7 @@ class DataTransformation:
             cat_pipeline = Pipeline(
                 steps=[
                     ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder(sparse_output=True)),
+                    ("one_hot_encoder", OneHotEncoder(sparse_output=True, handle_unknown='ignore')),
                     ("scaler", StandardScaler(with_mean=False))
 
                 ]
